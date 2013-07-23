c72134c2509c30a1528a18511797f61103cfae0e
Grahame Bowland <grahame@angrygoats.net>
2013-07-24 00:52:37 +0800

    cope with lower case

--- code/roman.py
+++ code/roman.py
@@ -1,8 +1,9 @@
-# source: http://rosettacode.org/wiki/Roman_numerals/Decode#Python
+# source, h/t: http://rosettacode.org/wiki/Roman_numerals/Decode#Python
 
 _rdecode = dict(zip('MDCLXVI', (1000, 500, 100, 50, 10, 5, 1)))
  
-def decode( roman ):
+def decode(roman):
+    roman = roman.upper()
     result = 0
     for r, r1 in zip(roman, roman[1:]):
         rd, rd1 = _rdecode[r], _rdecode[r1]
@@ -10,5 +11,5 @@ def decode( roman ):
     return result + _rdecode[roman[-1]]
  
 if __name__ == '__main__':
-    for r in 'MCMXC MMVIII MDCLXVI'.split():
+    for r in 'iii MCMXC MMVIII MDCLXVI'.split():
         print( r, decode(r) )
